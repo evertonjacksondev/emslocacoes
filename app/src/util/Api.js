1e6b3037079a315f979cc40db2b8fa0ae7be63cc
Everton Pudim Jackson <everton@debian-BULLSEYE-live-builder-AMD64>
2022-10-01 11:36:17 -0300

    created collection catalog

--- app/src/util/Api.js
+++ app/src/util/Api.js
@@ -1,29 +1,30 @@
 const axios = require('axios');
 
+
 export const getReserva = (success, error) => {
 
-    const configAxios = {
-      method: 'get',
-      timeout: 20000,
-      url: "https://ironrest.herokuapp.com/ems-reserva",
-    };
-  
-  
-    axios(configAxios).then((response)=>{ success(response.data) }).catch((err) => { error(err.response ? err.response.data : err) });
-  }
+  const configAxios = {
+    method: 'get',
+    timeout: 20000,
+    url: "https://ironrest.herokuapp.com/ems-reserva",
+  };
 
-  export const getReservaid = (_id, success, error) => {
 
-    const configAxios = {
-      method: 'get',
-      timeout: 20000,
-      url: `https://ironrest.herokuapp.com/ems-reserva/${_id}`,
-    };
+  axios(configAxios).then((response) => { success(response.data) }).catch((err) => { error(err.response ? err.response.data : err) });
+};
 
-    axios(configAxios).then((response)=>{ success(response.data) }).catch((err) => { error(err.response ? err.response.data : err) });
-}
+export const getReservaId = (_id, success, error) => {
 
-export const deleteReservaid = (_id, success, error) => {
+  const configAxios = {
+    method: 'get',
+    timeout: 20000,
+    url: `https://ironrest.herokuapp.com/ems-reserva/${_id}`,
+  };
+
+  axios(configAxios).then((response) => { success(response.data) }).catch((err) => { error(err.response ? err.response.data : err) });
+};
+
+export const deleteReservaId = (_id, success, error) => {
 
   const configAxios = {
     method: 'delete',
@@ -31,9 +32,8 @@ export const deleteReservaid = (_id, success, error) => {
     url: `https://ironrest.herokuapp.com/ems-reserva/${_id}`,
   };
 
-  axios(configAxios).then((response)=>{ success(response.data) }).catch((err) => { error(err.response ? err.response.data : err) });
-}
-
+  axios(configAxios).then((response) => { success(response.data) }).catch((err) => { error(err.response ? err.response.data : err) });
+};
 
 export const postCreateReserva = (body, success, error) => {
   /*{
@@ -58,10 +58,10 @@ export const postCreateReserva = (body, success, error) => {
   };
 
 
-  axios(configAxios).then((response)=>{ success(response.data) }).catch((err) => { error(err.response ? err.response.data : err) });
-}
+  axios(configAxios).then((response) => { success(response.data) }).catch((err) => { error(err.response ? err.response.data : err) });
+};
 
-export const putReservaid = (_id, body, success, error) => {
+export const putReservaId = (_id, body, success, error) => {
   /*{
     "Category": "",
     "Brand": "",
@@ -75,7 +75,7 @@ export const putReservaid = (_id, body, success, error) => {
     "PhoneNumber": "",
     "Address": ""
   }*/
-  
+
   const configAxios = {
     method: 'get',
     timeout: 20000,
@@ -83,5 +83,97 @@ export const putReservaid = (_id, body, success, error) => {
     data: body,
   };
 
-  axios(configAxios).then((response)=>{ success(response.data) }).catch((err) => { error(err.response ? err.response.data : err) });
-}
+  axios(configAxios).then((response) => { success(response.data) }).catch((err) => { error(err.response ? err.response.data : err) });
+};
+
+export const getCatalog = (success, error) => {
+
+  const configAxios = {
+    method: 'get',
+    timeout: 20000,
+    url: "https://ironrest.herokuapp.com/ems-reserva",
+  };
+
+
+  axios(configAxios).then((response) => { success(response.data) }).catch((err) => { error(err.response ? err.response.data : err) });
+};
+
+export const getCatalogId = (_id, success, error) => {
+
+  const configAxios = {
+    method: 'get',
+    timeout: 20000,
+    url: `https://ironrest.herokuapp.com/ems-reserva/${_id}`,
+  };
+
+  axios(configAxios).then((response) => { success(response.data) }).catch((err) => { error(err.response ? err.response.data : err) });
+};
+
+export const deleteCatalogId = (_id, success, error) => {
+
+  const configAxios = {
+    method: 'delete',
+    timeout: 20000,
+    url: `https://ironrest.herokuapp.com/ems-Catalog/${_id}`,
+  };
+
+  axios(configAxios).then((response) => { success(response.data) }).catch((err) => { error(err.response ? err.response.data : err) });
+};
+
+export const postCreateCatalog = (body, success, error) => {
+
+  try {
+
+    if (body.category) throw 'required Category'
+    if (body.brand) throw 'required Brand '
+    if (body.model) throw 'required Model '
+    if (body.dateOfWithdraw) throw 'required DateOfWithdraw '
+    if (body.dateOfDevolution) throw 'required DateOfDevolution '
+    if (body.price) throw 'required Price '
+    if (body.plate) throw 'required Plate '
+    if (body.name) throw 'required Name '
+    if (body.zipCode) throw 'required ZipCode '
+    if (body.phoneNumber) throw 'required PhoneNumber '
+    if (body.address) throw 'required Address'
+
+    const configAxios = {
+      method: 'post',
+      timeout: 20000,
+      url: "https://ironrest.herokuapp.com/ems-catalog",
+      data: body,
+    };
+    axios(configAxios).then((response) => { success(response.data) }).catch((err) => { error(err.response ? err.response.data : err) });
+
+  } catch (error) {
+    error(error.response ? error.response.data : error);
+  }
+};
+
+export const putCatalogId = (_id, body, success, error) => {
+  try {
+
+    if (body.category) throw 'required Category'
+    if (body.brand) throw 'required Brand '
+    if (body.model) throw 'required Model '
+    if (body.dateOfWithdraw) throw 'required DateOfWithdraw '
+    if (body.dateOfDevolution) throw 'required DateOfDevolution '
+    if (body.price) throw 'required Price '
+    if (body.plate) throw 'required Plate '
+    if (body.name) throw 'required Name '
+    if (body.zipCode) throw 'required ZipCode '
+    if (body.phoneNumber) throw 'required PhoneNumber '
+    if (body.address) throw 'required Address'
+
+    const configAxios = {
+      method: 'get',
+      timeout: 20000,
+      url: `https://ironrest.herokuapp.com/ems-catalog/${_id}`,
+      data: body,
+    };
+
+    axios(configAxios).then((response) => { success(response.data) }).catch((err) => { error(err.response ? err.response.data : err) });
+  } catch (error) {
+    error(error.response ? error.response.data : error);
+  }
+
+};
