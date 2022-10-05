6a1549656c1bd07c1bdde496beacfcef72cb623b
Everton Pudim Jackson <everton@debian-BULLSEYE-live-builder-AMD64>
2022-10-04 22:50:07 -0300

    ajust component muidatable

--- app/src/Pages/Reserva/Reserva.js
+++ app/src/Pages/Reserva/Reserva.js
@@ -1,8 +1,64 @@
+import MUIDataTable from 'mui-datatables';
 import React from 'react'
 
+
 const Reserva = () => {
+
+
+  const columns = [
+    {
+      name: "name",
+      label: "Name",
+      options: {
+        filter: true,
+        sort: true,
+      }
+    },
+    {
+      name: "company",
+      label: "Company",
+      options: {
+        filter: true,
+        sort: false,
+      }
+    },
+    {
+      name: "city",
+      label: "City",
+      options: {
+        filter: true,
+        sort: false,
+      }
+    },
+    {
+      name: "state",
+      label: "State",
+      options: {
+        filter: true,
+        sort: false,
+      }
+    },
+  ];
+
+  const data = [
+    { name: "Joe James", company: "Test Corp", city: "Yonkers", state: "NY" },
+    { name: "John Walsh", company: "Test Corp", city: "Hartford", state: "CT" },
+    { name: "Bob Herm", company: "Test Corp", city: "Tampa", state: "FL" },
+    { name: "James Houston", company: "Test Corp", city: "Dallas", state: "TX" },
+  ];
+
+  const options = {
+    filterType: 'checkbox',
+  };
+
+
   return (
-    <div>Reserva</div>
+
+    <MUIDataTable
+      title={"Employee List"}
+      data={data}
+      columns={columns}
+      options={options} />
   )
 }
 
