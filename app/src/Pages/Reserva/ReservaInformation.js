7eb4799eef2a0c5ac1b5056054f5cb328e9370d2
Everton Pudim Jackson <everton@debian-BULLSEYE-live-builder-AMD64>
2022-10-08 16:03:29 -0300

    adjust input uf

--- app/src/Pages/Reserva/ReservaInformation.js
+++ app/src/Pages/Reserva/ReservaInformation.js
@@ -269,7 +269,35 @@ const ReservaInformation = () => {
         </Grid>
 
         <Grid item lg={2} xs={6}>
-          <TextField value={uf} onChange={(e) => setUf(e.target.value)} size={"small"} fullWidth label={"Estado"}></TextField>
+          <TextField select value={uf} onChange={(e) => setUf(e.target.value)} size={"small"} fullWidth label={"Estado"}>
+            <MenuItem value={'AC'} >AC</MenuItem>
+            <MenuItem value={'AL'}>AL</MenuItem>
+            <MenuItem value={'AP'}>AP</MenuItem>
+            <MenuItem value={'AM'}>AM</MenuItem>
+            <MenuItem value={'BA'}>BA</MenuItem>
+            <MenuItem value={'CE'}>CE</MenuItem>
+            <MenuItem value={'DF'}>DF</MenuItem>
+            <MenuItem value={'ES'}>ES</MenuItem>
+            <MenuItem value={'GO'}>GO</MenuItem>
+            <MenuItem value={'MA'}>MA</MenuItem>
+            <MenuItem value={'MT'}>MT</MenuItem>
+            <MenuItem value={'MS'}>MS</MenuItem>
+            <MenuItem value={'MG'}>MG</MenuItem>
+            <MenuItem value={'PA'}>PA</MenuItem>
+            <MenuItem value={'PB'}>PB</MenuItem>
+            <MenuItem value={'PR'}>PR</MenuItem>
+            <MenuItem value={'PE'}>PE</MenuItem>
+            <MenuItem value={'PI'}>PI</MenuItem>
+            <MenuItem value={'RJ'}>RJ</MenuItem>
+            <MenuItem value={'RN'}>RN</MenuItem>
+            <MenuItem value={'RS'}>RS</MenuItem>
+            <MenuItem value={'RO'}>RO</MenuItem>
+            <MenuItem value={'RR'}>RR</MenuItem>
+            <MenuItem value={'SC'}>SC</MenuItem>
+            <MenuItem value={'SP'}>SP</MenuItem>
+            <MenuItem value={'SE'}>SE</MenuItem>
+            <MenuItem value={'TO'}>TO</MenuItem>
+          </TextField>
         </Grid>
 
         <Grid item lg={12} xs={12}>
