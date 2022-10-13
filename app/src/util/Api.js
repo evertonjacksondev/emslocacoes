ee5741dec1d86f6fbe089f937458c6c8c65bda59
Everton Pudim Jackson <everton@debian-BULLSEYE-live-builder-AMD64>
2022-10-13 19:24:15 -0300

    created delete catalog

--- app/src/util/Api.js
+++ app/src/util/Api.js
@@ -130,7 +130,7 @@ export const deleteCatalogId = (_id, success, error) => {
   const configAxios = {
     method: 'delete',
     timeout: 20000,
-    url: `${baseUrl}/ems-Catalog/${_id}`,
+    url: `${baseUrl}/ems-catalog/${_id}`,
   };
 
   axios(configAxios).then((response) => { success(response.data) }).catch((err) => { error(err.response ? err.response.data : err) });
@@ -155,20 +155,8 @@ export const postCreateCatalog = (body, success, error) => {
 export const putCatalogId = (_id, body, success, error) => {
   try {
 
-    if (!body.category) throw 'required Category'
-    if (!body.brand) throw 'required Brand '
-    if (!body.model) throw 'required Model '
-    if (!body.dateOfWithdraw) throw 'required DateOfWithdraw '
-    if (!body.dateOfDevolution) throw 'required DateOfDevolution '
-    if (!body.price) throw 'required Price '
-    if (!body.plate) throw 'required Plate '
-    if (!body.name) throw 'required Name '
-    if (!body.zipCode) throw 'required ZipCode '
-    if (!body.phoneNumber) throw 'required PhoneNumber '
-    if (!body.address) throw 'required Address'
-
     const configAxios = {
-      method: 'get',
+      method: 'put',
       timeout: 20000,
       url: `${baseUrl}/ems-catalog/${_id}`,
       data: body,
